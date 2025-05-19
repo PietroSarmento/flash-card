c77638be025d00581ea7b964ef19855747adb887
Pietro 2D <pietro.sarmento@escola.pr.gov.br>
2025-05-19 11:25:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,3 @@
+criaCartao("Programação","O que é Python?","Python é uma linguagem de programação");
+criaCartao("Geografia","Capital da França?","Paris");
+criaCartao("Inglês","Como se diz oi?","Hi");
